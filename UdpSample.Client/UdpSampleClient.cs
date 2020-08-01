125e43c2d8a49c437c9493831b4d2b40620024d6
KoHey94@X1C <sugawara622@gmail.com>
2020-08-01 23:33:13 +0900

    UDP/IP通信のプログラムを書いた

---
+++ UdpSample.Client/UdpSampleClient.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+namespace UdpSample.Client
+{
+    class UdpSampleClient
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Udp Client.");
+
+            var sendIp = "127.0.0.1";
+            var sendPort = 12345;
+
+            using var udpClient = new UdpClient();
+            while (true)
+            {
+                var sendMsg = Console.ReadLine();
+                var sendBytes = Encoding.UTF8.GetBytes(sendMsg);
+
+                udpClient.Send(sendBytes, sendBytes.Length, sendIp, sendPort);
+                
+                if (sendMsg == "exit") break;
+            }
+            udpClient.Close();            
+        }
+    }
+}
---
+++ UdpSample.Server/UdpSampleServer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace UdpSample.Server
+{
+    class UdpServer
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Udp Server.");
+
+            var receiveIp = "127.0.0.1";
+            var receivePort = 12345;
+
+            using var udpReceive = new UdpClient(
+                new IPEndPoint(IPAddress.Parse(receiveIp), receivePort));
+            
+            while (true)
+            {
+                IPEndPoint remoteEp = null;
+                var receiveBytes = udpReceive.Receive(ref remoteEp);
+
+                var receiveMsg = Encoding.UTF8.GetString(receiveBytes);
+
+                Console.WriteLine($"receiveData   : {receiveMsg}");
+                Console.WriteLine($"remoteAddress : {remoteEp.Address}");
+                Console.WriteLine($"remotePort    : {remoteEp.Port}");
+
+                if (receiveMsg == "exit") break;
+            }
+            udpReceive.Close();
+        }
+    }
+}
