b27508eccf4b600551f9012cfa84039ddd7627c0
KoHey94@X1C <sugawara622@gmail.com>
2020-08-10 00:13:10 +0900

    _

--- BlazorDemo/Pages/App100.razor.cs
+++ BlazorDemo/Pages/App100.razor.cs
@@ -1,22 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace BlazorDemo.Pages
 {
     public partial class App100
     {
+        private TimeSpan タイマー = new TimeSpan();
+        private bool タイマー起動中 = false;
+        private async Task ストップウォッチ()
+        {
+            タイマー起動中 = true;
+            while (タイマー起動中)
+            {
+                await Task.Delay(1000);
+                if (タイマー起動中)
+                {
+                    タイマー = タイマー.Add(new TimeSpan(0, 0, 1));
+                    StateHasChanged();
+                }
+            }
+        }
+
         private string 入力文字列 { get; set; } = string.Empty;
 
         private string ランダム文字列 { get; set; } = string.Empty;
 
         private string 正誤判定 { get; set; } = string.Empty;
 
-        System.Diagnostics.Stopwatch sw;
-        private string タイマー { get; set; }
-
-        private void 文字列生成()
+        private async void 文字列生成()
         {
             var 長さ = 20;
             var 半角英数字 = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLNMOPQRSTUVWXYZ";
@@ -34,6 +45,9 @@ namespace BlazorDemo.Pages
             }
 
             ランダム文字列 = 生成文字列.ToString();
+            正誤判定 = string.Empty;
+            タイマー = new TimeSpan();
+            await ストップウォッチ();
         }
 
         private void 判定()
@@ -41,6 +55,7 @@ namespace BlazorDemo.Pages
             if (入力文字列 == ランダム文字列)
             {
                 正誤判定 = "正解";
+                タイマー起動中 = false;
             }
             else
             {
