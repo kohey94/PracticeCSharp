4f8e740627eb42f56bb13015acaf82eac1cb593b
KoHey94@X1C <sugawara622@gmail.com>
2020-08-16 20:24:58 +0900

    _

--- RazorPagesDemo/Pages/app001.cshtml.cs
+++ RazorPagesDemo/Pages/app001.cshtml.cs
@@ -9,6 +9,7 @@ namespace RazorPagesDemo.Pages
 {
     public class App001Model : PageModel
     {
+        // https://github.com/JustSystems/java-100practices/blob/master/contents/100/README.md
         public void OnGet()
         {
 
