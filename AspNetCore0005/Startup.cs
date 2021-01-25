fc14356baf2f4713b72ecd36a111eb18909b4d7b
KoHey94@X1C <sugawara622@gmail.com>
2021-01-26 00:44:43 +0900

    MVCサービスの説明追加

--- AspNetCore0005/Startup.cs
+++ AspNetCore0005/Startup.cs
@@ -21,7 +21,28 @@ namespace AspNetCore0005
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseMvcWithDefaultRoute();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+            //以下も同じ意味
+            //app.UseMvcWithDefaultRoute();
+
+
+
+            // ルートの設定なし
+            // app.UseMvc(routes => { });
+            // 以下も同じ意味
+            // appd.UseMvc();
+
+            // ルートの設定がされていないURLにアクセスした場合は下の処理が実行される。
+            app.Run(async context =>
+            {
+                await context.Response.WriteAsync(
+                    "I'd rather say there are no configured routes here.");
+            });
         }
     }
 }
--- AspNetCore0005/memo.md
+++ AspNetCore0005/memo.md
@@ -1,1 +1,9 @@
 ﻿# ASP.NET Core MVCメモ
+## AddMvcCoreとAddMvc
+AddMvcCoreは最小限のMVCサービスを追加する。AddMvcは全てのMVCサービスを追加する。  
+参考:https://qiita.com/sengoku/items/6c30d1de09a62e28348f  
+
+## UseMvc
+MVCサービスを追加したらUseMvcを使い、ルーティングを有効化する。  
+UseMvcWithDefaultRouteでデフォルトのルートを設定できる。  
+UseMvcにパラメーターを渡さなくてもアプリケーションは起動するが、ルートの設定がされていないため404エラーになる。  
