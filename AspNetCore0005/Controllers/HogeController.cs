c35647f092459665a7b614ed5afc69df72eb4dc5
KoHey94@X1C <sugawara622@gmail.com>
2021-01-28 14:05:41 +0900

    属性ルーティングについて書いた

---
+++ AspNetCore0005/Controllers/HogeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCore0005.Controllers
+{
+    [Route("hoge")]
+    public class HogeController : Controller
+    {
+        [Route("index")]
+        public IActionResult Index()
+        {
+            return new ContentResult { Content = "Hoge" };
+        }
+
+        [Route("hogepage")]
+        public IActionResult HogePage()
+        {
+            return new ContentResult { Content = $"HogePage !!!!!!!!!!!!!!" };
+        }
+
+        [Route("hogepage/{value}")]
+        public IActionResult HogePage(string value)
+        {
+            return new ContentResult { Content = $"HogePage {value}"  };
+        }
+    }
+}
